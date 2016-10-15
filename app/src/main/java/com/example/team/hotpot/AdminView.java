5942bdcb17f6670af3f7fb36e36ec2f6a2648e0d
Annie Myers <amyers@cse.local>
2016-10-15 09:38:22 -0400

    implemented admin music list

--- app/src/main/java/com/example/team/hotpot/AdminView.java
+++ app/src/main/java/com/example/team/hotpot/AdminView.java
@@ -1,32 +1,21 @@
 package com.example.team.hotpot;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class EditPlaylist extends AppCompatActivity {
+public class AdminView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_playlist);
-
+        setContentView(R.layout.activity_admin_view);
         String[] mobileArray = {"Closer - The Chainsmokers","Starboy - The Weekend","Heathens - twenty one pilots","Cold Water - Major Lazer","Let Me Love You - DJ Snake","Broccoli - D.R.A.M.","Treat You Better - Shawn Mendes","Cheap Thrills - Sia"};
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_playlist, mobileArray);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_admin_view, mobileArray);
 
-        ListView listView = (ListView) findViewById(R.id.edit_playlist);
+        ListView listView = (ListView) findViewById(R.id.music_list);
         listView.setAdapter(adapter);
-
-        Intent intent = getIntent();
-    }
-
-    public void goBack(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/team/hotpot/JoinRoom.java
+++ app/src/main/java/com/example/team/hotpot/JoinRoom.java
@@ -1,11 +1,15 @@
 package com.example.team.hotpot;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class JoinRoom extends AppCompatActivity {
+    Button b1;
+    private ProgressDialog progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +17,39 @@ public class JoinRoom extends AppCompatActivity {
         setContentView(R.layout.activity_join_room);
 
         Intent intent = getIntent();
+
+        b1 = (Button) findViewById(R.id.button9);
+
+    }
+
+    public void loading(View view){
+        progress=new ProgressDialog(this);
+        progress.setMessage("Joining server...");
+        progress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progress.setIndeterminate(true);
+        progress.setProgress(0);
+        progress.show();
+
+        final int totalProgressTime = 100;
+        final Thread t = new Thread() {
+            @Override
+            public void run() {
+                int jumpTime = 0;
+
+                while(jumpTime < totalProgressTime) {
+                    try {
+                        sleep(200);
+                        jumpTime += 5;
+                        progress.setProgress(jumpTime);
+                    }
+                    catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t.start();
     }
 
     public void goBack(View view) {
